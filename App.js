2ebd1e229f405e2755edeb6ac6c6fc66197bc5f1
Gabriel Duquette <gabrielduquette@gmail.com>
2020-11-01 16:27:38 -0500

    make work on mobile

--- App.js
+++ App.js
@@ -5,9 +5,15 @@ import Svg, { Circle, Defs, Pattern, Rect, Path } from "react-native-svg";
 const App = () => {
   const drawTarget = () => {
     return (
-      <Svg height="100%" width="100%" viewBox="0 0 100.25 100.25">
-        {[1, 2, 3, 4, 5].map((r) => (
-          <Circle cx="50" cy="50" r={r * 10} fill="rgb(255, 0, 0, 0.1)" />
+      <Svg height="100%" width="100%" viewBox="0 0 100 100">
+        {[1, 2, 3, 4, 5].map((r, i) => (
+          <Circle
+            key={i}
+            cx="50"
+            cy="50"
+            r={r * 10}
+            fill="rgb(255, 0, 0, 0.1)"
+          />
         ))}
         <Defs>
           <Pattern
@@ -20,25 +26,18 @@ const App = () => {
               d="M 10 0 L 0 0 0 10"
               fill="none"
               stroke="gray"
-              stroke-width="0.5"
+              stroke-width="1"
             />
           </Pattern>
         </Defs>
-        <Rect width="100.25" height="100.25" fill="url(#grid)" />
+        <Rect width="100" height="100" fill="url(#grid)" />
       </Svg>
     );
   };
 
   return (
     <View style={styles.container}>
-      <div
-        style={{
-          height: "500px",
-          width: "500px",
-        }}
-      >
-        {drawTarget()}
-      </div>
+      <View style={styles.innerContainer}>{drawTarget()}</View>
     </View>
   );
 };
@@ -49,6 +48,10 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+  },
+  innerContainer: {
+    height: 301.5,
+    width: 301.5,
   },
 });
 
