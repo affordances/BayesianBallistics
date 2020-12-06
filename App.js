7fa0e459718304484a00915a3a0563d9564d01b7
Gabriel Duquette <gabrielduquette@gmail.com>
2020-12-06 18:21:01 -0500

    get example code working by itself

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import {
   Button,
+  Dimensions,
   Keyboard,
   Pressable,
   StyleSheet,
@@ -15,12 +16,51 @@ import Svg, {
   Rect,
   Path,
   Text,
-  TSpan,
 } from "react-native-svg";
+import Animated from "react-native-reanimated";
+import { PanGestureHandler, State } from "react-native-gesture-handler";
+
+const { width, height } = Dimensions.get("window");
+
+const { cond, eq, add, set, Value, event, interpolate, Extrapolate } = Animated;
 
 const App = () => {
   const [shots, setShots] = useState([]);
   const [distance, setDistance] = useState("");
+
+  const dragX = new Value(0);
+  const dragY = new Value(0);
+  const offsetX = new Value(width / 2);
+  const offsetY = new Value(height / 2);
+  const gestureState = new Value(-1);
+  const onGestureEvent = event([
+    {
+      nativeEvent: {
+        translationX: dragX,
+        translationY: dragY,
+        state: gestureState,
+      },
+    },
+  ]);
+  const transX = cond(
+    eq(gestureState, State.ACTIVE),
+    add(offsetX, dragX),
+    set(offsetX, add(offsetX, dragX)),
+  );
+  const transY = cond(
+    eq(gestureState, State.ACTIVE),
+    add(offsetY, dragY),
+    set(offsetY, add(offsetY, dragY)),
+  );
+  const borderWidth = interpolate(transX, {
+    inputRange: [0, width],
+    outputRange: [0, 5],
+    extrapolate: Extrapolate.CLAMP
+  });
+  const opacity = interpolate(transY, {
+    inputRange: [0, height],
+    outputRange: [0.1, 1],
+  });
 
   const drawTarget = () => {
     return (
@@ -72,7 +112,7 @@ const App = () => {
                 fontSize="4"
                 fontWeight={300}
               >
-                {i + 1} poop
+                {i + 1}
               </Text>
             </G>
           ))}
@@ -83,28 +123,62 @@ const App = () => {
 
   return (
     <View style={styles.container}>
-      <TextInput
+      <PanGestureHandler
+          maxPointers={1}
+          onGestureEvent={onGestureEvent}
+          onHandlerStateChange={onGestureEvent}
+        >
+          <Animated.View
+            style={[
+              styles.box,
+              {
+                opacity: opacity,
+                borderWidth: borderWidth,
+                transform: [
+                  {
+                    translateX: transX,
+                  },
+                  {
+                    translateY: transY,
+                  },
+                ],
+              },
+            ]}
+          />
+        </PanGestureHandler>
+      {/* <TextInput
         keyboardType="numeric"
         style={{ height: 40, width: 200, borderColor: "gray", borderWidth: 1 }}
         onChangeText={(text) => setDistance(text)}
         value={distance}
       />
       <View style={styles.innerContainer}>{drawTarget()}</View>
-      <Button title="Reset" onPress={() => setShots([])} />
+      <Button title="Reset" onPress={() => setShots([])} /> */}
     </View>
   );
 };
 
+const CIRCLE_SIZE = 70;
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    // backgroundColor: "#fff",
+    // alignItems: "center",
+    // justifyContent: "center",
   },
   innerContainer: {
     height: 301.5,
     width: 301.5,
+  },
+  box: {
+    backgroundColor: "tomato",
+    marginLeft: -(CIRCLE_SIZE / 2),
+    marginTop: -(CIRCLE_SIZE / 2),
+    width: CIRCLE_SIZE,
+    height: CIRCLE_SIZE,
+    borderRadius: CIRCLE_SIZE / 2,
+    borderColor: "#000"
   },
 });
 
